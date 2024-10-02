b93f3a400ec11b3d606e92e1bb90095b5a2f9468
himeko24 <stefanusandrian4@gmail.com>
2024-10-02 09:54:51 +0700

    added for week 6 tutorial

--- App.js
+++ App.js
@@ -1,11 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+// const Input = () => {
+//   const [name, setName] = useState("");
+//   const handleChangeMyName = (value) => {
+//     setName(value);;
+//   };
+
+//   console.log(name);
+
+//   return (
+//     <View>
+//       <Text>Name</Text>
+//       <TextInput placeholder='Input your name'
+//         style={{
+//           borderColor: "black",
+//           borderWidth: 1,
+//           padding: 10,
+//           borderRadius: 8,
+//         }}
+//         onChangeText={handleChangeMyName}
+//       />
+//     </View>
+//   );
+// };
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Stefanus Andrian Leoandi - 00000055662</Text>
+//       <Input />
+//     </View>
+//   );
+// }
+
+const Input = ({ name, onChangeText }) => {
+  // console.log(name);
+
+  return (
+    <View>
+      <Text>Name</Text>
+      <TextInput placeholder="Input your name"
+        style={{
+          borderColor: "black",
+          borderWidth: 1,
+          padding: 10,
+          borderRadius: 8,
+        }}
+        onChangeText={onChangeText}
+      />
+    </View>
+  );
+};
+
+const NumberInput = ({ nim, onChangeText }) => {
+  // console.log(nim);
+
+  return (
+    <View>
+      <Text>NIM</Text>
+      <TextInput placeholder="Input your NIM"
+        style={{
+          borderColor: "black",
+          borderWidth: 1,
+          padding: 10,
+          borderRadius: 8,
+        }}
+        onChangeText={onChangeText}
+        keyboardType="numeric"
+      />
+    </View>
+  );
+}
 
 export default function App() {
+  const [name, setName] = useState("");
+  const handleChangeMyName = (nameValue) => {
+    setName(nameValue);
+  }
+
+  const [nim, setNIM] = useState("");
+  const handleChangeMyNIM = (NIMValue) => {
+    setNIM(NIMValue)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{name} - {nim}</Text>
+      <Input name={name} onChangeText={handleChangeMyName} />
+      <NumberInput nim={nim} onChangeText={handleChangeMyNIM} />
     </View>
   );
 }
